c15a9ffd617a1f70438236b695387296a280477c
Zamudio2024 <fabianzamudio38@aragon.unam.mx>
2020-04-05 22:31:35 -0500

    Ejercicios

--- Practicas/Practica_4/src/practica_4/Practica_4.java
+++ Practicas/Practica_4/src/practica_4/Practica_4.java
@@ -5,6 +5,8 @@
  */
 package practica_4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Fabian
@@ -14,8 +16,35 @@ public class Practica_4 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { //<>
+        
+        char band = 'T';
+        int sumser = 0, I =2;
+        
+        while (I <= 1800) {
+            
+            sumser += I;
+            
+            System.out.println("El valor de I es: "+ I);
+            
+            if (band == 'T') {
+                
+                band = 'F';
+                I+= 3;
+                
+            }else{
+                
+                band = 'T';
+                I += 2;
+                
+                
+            }
+            
+            
+            
+        }
+        
+        System.out.println("La suma de la series es: "+ sumser);
     }
     
 }
